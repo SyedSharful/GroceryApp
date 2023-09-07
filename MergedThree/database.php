bb5dced72847c8cb806e5d3bda9dddfff4dab671
Kevin Zhou <89994825+klz4@users.noreply.github.com>
2023-09-06 21:07:48 -0400

    Update database.php

--- MergedThree/database.php
+++ MergedThree/database.php
@@ -1,13 +1,11 @@
 <?php
-	  $servername = "sql1.njit.edu";
-    $username = "klz4";
-    $password = "K4419Spark!";
-    $dbname = "klz4"; 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+	$servername = "localhost";
+	$username = "root";
+    	$password = "";
+    	$dbname = "cs490"; 
+    	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    if (!$conn)
-    {
-        die("Connection error: " . mysqli_connect_errno());
-    }
+    	if (!$conn){
+        	die("Connection error: " . mysqli_connect_errno());
+    	}
 ?>
-    
