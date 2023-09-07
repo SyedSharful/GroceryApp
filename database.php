14e649114f94b0e69c0108797dd78fde8ae7946c
Kevin Zhou <89994825+klz4@users.noreply.github.com>
2023-09-06 21:05:13 -0400

    Update database.php

--- database.php
+++ database.php
@@ -10,13 +10,3 @@
     	}
 ?>
     
-<!--$servername = "sql1.njit.edu";
-    $username = "klz4";
-    $password = "K4419Spark!";
-    $dbname = "klz4"; 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-    if (!$conn)
-    {
-        die("Connection error: " . mysqli_connect_errno());
-    }-->
